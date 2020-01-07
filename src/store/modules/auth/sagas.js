811af03cbbc06702d0eb30c4330791b4bdd286a8
root <root@DESKTOP-41CTVRE.localdomain>
2020-01-06 22:28:19 -0200

    Cadastro de usuário na aplicação

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -29,4 +29,25 @@ export function* signIn({ payload }) {
     yield put(signFailure());
   }
 }
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    history.push('/');
+  } catch (err) {
+    toast.error('Falha em cadastrar o usuário, verifique os dados.');
+    yield put(signFailure());
+  }
+}
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
